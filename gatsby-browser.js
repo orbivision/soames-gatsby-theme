af1cbde2b8841a7d0be9339d8b7208d96e5c1567
John Eric Fritz <fritz@orbisoftware.com>
2025-04-12 19:15:20 -0500

    Added Soames and vendoer CSS files

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+// Vendor styles only
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "@popperjs/core/dist/umd/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+//import "./src/styles/vendor/normalize.css";
+import "./src/styles/vendor/wordpress-blocks.css";
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import '../styles/global.css';
+import '../styles/theme.css';
 
 type LayoutProps = {
   children: ReactNode;
