bf90cb4dd8564e37a2909f31e35b7604d937965f
John Eric Fritz <fritz@orbisoftware.com>
2025-05-31 19:11:15 -0500

    Added remaining navigation components

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,7 @@ import "@popperjs/core/dist/umd/popper.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 //import "./src/styles/vendor/normalize.css";
 import "./src/styles/vendor/wordpress-blocks.css";
+
+// Soames - custom JS
+import "./static/js/soames-nav-dropdown.js";
+import "./static/js/soames-navbar-dropdown.js";
---
+++ gatsby-ssr.js
@@ -0,0 +1,12 @@
+const React = require("react")
+
+export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  setHeadComponents([
+    <script
+      key="jquery-for-ssr"
+      src="https://code.jquery.com/jquery-3.4.1.min.js"
+      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+      crossOrigin="anonymous">
+    </script>,
+  ])
+}
