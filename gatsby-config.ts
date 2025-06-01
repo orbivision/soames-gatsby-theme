50be2ac319b23026ca3255931d0ce3964eb2a896
John Eric Fritz <fritz@orbisoftware.com>
2025-06-01 16:50:08 -0500

    Removed some plugins that should be loaded in starter instead of theme

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,32 +22,6 @@ const themeConfig = (themeOptions: ThemeOptions = {}): GatsbyConfig => {
           url,
         },
       },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `assets`,
-          path: `${__dirname}/content/assets`,
-        },
-      },
-      `gatsby-plugin-image`,
-      `gatsby-plugin-sharp`,
-      `gatsby-transformer-sharp`,
-      {
-        // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
-        resolve: `gatsby-plugin-manifest`,
-        options: {
-          name: `Gatsby Starter WordPress Blog`,
-          short_name: `GatsbyJS & WP`,
-          start_url: `/`,
-          background_color: `#ffffff`,
-          theme_color: `#663399`,
-          display: `minimal-ui`,
-          icon: `content/assets/gatsby-icon.png`,
-        },
-      },
-
-      // See https://www.gatsbyjs.com/plugins/gatsby-plugin-react-helmet/?=gatsby-plugin-react-helmet
-      `gatsby-plugin-react-helmet`,
     ],
   };
 };
